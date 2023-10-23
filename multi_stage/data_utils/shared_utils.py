3f9bde50c6b3a415647f96707060792f0bb478dd
duyvu1110 <100179213+duyvu1110@users.noreply.github.com>
2023-10-24 02:07:43 +0700

    .

--- multi_stage/data_utils/shared_utils.py
+++ multi_stage/data_utils/shared_utils.py
@@ -498,10 +498,10 @@ def token_mapping_bert(bert_token_col, gold_token_col):
                     bert_length -= 2
                 
                         
-            print(bert_length, token_length)
+            # print(bert_length, token_length)
             if(bert_length!=token_length):
-                print(seq_bert_token[bert_index])
-                print(seq_gold_token[token_index])
+                print(seq_bert_token)
+                print(seq_gold_token)
             assert bert_length == token_length, "appear mapping error!"
             # check_utils.check_mapping_process(seq_map, seq_gold_token, seq_bert_token)
 
